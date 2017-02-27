16175e059ec8783ab38a0fb76020f49e05d9e4d5
MichaelLeonTreat <michael.leon.treat@gmail.com>
2017-02-27 12:41:57 -0800

    added event listener to the form data. Added logic to do something if the user's input is equal to the answer or not.

--- math-challenge.js
+++ math-challenge.js
@@ -2,18 +2,34 @@
 
 // test function to prepare us for html page.
 
-var x = 3;
-var y = 5;
+var banana = 3;
+var wizardHat = 5;
+
+var broom = wizardHat;
+var peel = banana;
 
 function riddleMeThis(x ,y){
   var number = x + y;
   return number;
 }
 
-var answerToRiddle = answer(x, y);
+var broom = wizardHat;
+var peel = banana;
+var answerToRiddle = riddleMeThis(broom, peel );
 
-if( 7 === answerToRiddle){
-  location.href = 'index.html'; // place-holder
-}else {
-  alert('incorrect');
-};
+var formEl = document.getElementById('answer-form');
+
+formEl.addEventListener('submit', checkAnswer);
+
+function checkAnswer(event){
+  event.stopPropagation();
+  event.preventDefault();
+
+  var userInput = parseInt(formEl.userInput.value);
+  console.log(userInput);
+  if( userInput === answerToRiddle){
+    location.href = 'index.html'; // place-holder
+  }else {
+    alert('incorrect');
+  };
+}
