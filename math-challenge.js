2ba86492d00051aea80af6eee70128fd7df3e621
MichaelLeonTreat <michael.leon.treat@gmail.com>
2017-02-27 12:18:05 -0800

    added functionality to the js page, added form template, added intro and riddles to html.

---
+++ math-challenge.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// test function to prepare us for html page.
+
+var x = 3;
+var y = 5;
+
+function answer(x ,y){
+  var number = x + y;
+  return number;
+}
+
+var answerToRiddle = answer(x, y);
+
+if( 7 === answerToRiddle){
+  location.href = 'index.html'; // place-holder
+}else {
+  alert('incorrect');
+};
