dd67050fee7cb5013e45463e8fd3d200b0bcae85
Kevin Schmidt <NSPtuning@gmail.com>
2017-02-27 16:34:57 -0800

    question event handle done

--- rpg1.js
+++ rpg1.js
@@ -1,19 +1,42 @@
 'use strict';
 
+// var userName;
+//
+// if(localStorage.userName){
+//   console.log(localStorage.userName);
+//   userName = JSON.parse(localStorage.userName);
+// }else{
+//   userName = prompt('Hmmmm looks like we forgot your name. What was it again?');
+//   localStorage.userName = userName;
+// };
+
+// Question One Variables
 var questionOne = 'What is a "for" loop?';
-var questionThree = '';
-
 var answerOne = 'to count things';
-
-var answerThree = 'your mom';
-
 var answerOneIncorrectOne = 'testing321';
 var answerOneIncorrectTwo = 'testing123';
 
-// question Two
+// Question Two Variables
 var questionTwo = 'What is the meaning of life?';
 var answerTwo = 'something stupid';
 
+// Question Three Variable
+var questionThree = '';
+var answerThree = 'your mom';
+
+// Handle correct answer click
+function handleClick(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  console.log(event.target);
+
+  if (event.target.id === 'correctAnswer') {
+    questionTwoEl();
+  }
+};
+
+// Question One
 var questionOneEl = function() {
   var questionEl = document.getElementById('question');
   question.textContent = questionOne;
@@ -21,18 +44,27 @@ var questionOneEl = function() {
   var answerEl = document.getElementById('possibleAnswers');
 
   var answerOneEl = document.createElement('li');
+  answerOneEl.setAttribute('id', 'correctAnswer');
+  console.log(answerOneEl.id);
   answerEl.appendChild(answerOneEl);
   answerOneEl.textContent = answerOne;
+  answerOneEl.addEventListener('click', handleClick);
 
   var answerTwoEl = document.createElement('li');
+  answerTwoEl.setAttribute('id', 'incorrectAnswer');
   answerEl.appendChild(answerTwoEl);
   answerTwoEl.textContent = answerOneIncorrectOne;
+  answerTwoEl.addEventListener('click', handleClick);
 
   var answerThreeEl = document.createElement('li');
+  answerThreeEl.setAttribute('id', 'incorrectAnswer');
   answerEl.appendChild(answerThreeEl);
   answerThreeEl.textContent = answerOneIncorrectTwo;
+  answerThreeEl.addEventListener('click', handleClick);
 
 };
+
+// Question Two
 
 var questionTwoEl = function() {
   var questionEl = document.getElementById('question');
@@ -40,17 +72,11 @@ var questionTwoEl = function() {
 
 };
 
+// Question Three
+
 var questionThreeEl = function() {
   var questionEl = document.getElementById('question');
   question.textContent = questionTwo;
-};
-
-function handleClick(event) {
-  event.preventDefault();
-  event.stopPropagation();
-
-  if (event.target.parentNode.id = 'correctAnswer');
-
 };
 
 questionOneEl();
