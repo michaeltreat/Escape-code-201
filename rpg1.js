d48f3a2697973ffa7035f660fca81b46edd1032b
Kevin Schmidt <NSPtuning@gmail.com>
2017-02-28 11:27:05 -0800

    three questions all dun

--- rpg1.js
+++ rpg1.js
@@ -11,6 +11,9 @@
 //   userName = prompt('Hmmmm looks like we forgot your name. What was it again?');
 //   localStorage.userName = userName;
 // };
+var incorrectNumber = 0;
+
+var answerEl = document.getElementById('possibleAnswers'); //correct answer for event listener
 
 // Question One Variables
 var questionOne = 'What is a "for" loop?';
@@ -20,11 +23,27 @@ var answerOneIncorrectTwo = 'testing123';
 
 // Question Two Variables
 var questionTwo = 'What is the meaning of life?';
-var answerTwo = 'something stupid';
+var answerTwo = 'answer two';
+var answerTwoIncorrectOne = '22222incorrect answer one22222';
+var answerTwoIncorrectTwo = '222222incorrect answer two22222';
 
 // Question Three Variable
-var questionThree = '';
+var questionThree = 'Question Three';
 var answerThree = 'your mom';
+var answerThreeIncorrectOne = '333333 incorrect answer one33333';
+var answerThreeIncorrectTwo = '333333 incorrect answer two33333';
+
+// total questions wrong and messages
+
+var questionsWrong = function () {
+  if (incorrectNumber < 2) {
+    confirm('you passed');
+  } else if (incorrectNumber > 2 && incorrectNumber < 4) {
+    confirm('you passed. BARELY!');
+  } else {
+    confirm('You got ' + incorrectNumber + ' wrong.  YOU FAIL!!');
+  }
+};
 
 // Handle correct answer click
 function handleClick(event) {
@@ -33,8 +52,17 @@ function handleClick(event) {
 
   console.log(event.target);
 
-  if (event.target.id === 'correctAnswer') {
+  if (event.target.id === 'correctAnswerOne') {
     questionTwoEl();
+  } else if (event.target.id === 'correctAnswerTwo') {
+    questionThreeEl();
+  } else if (event.target.id === 'correctAnswerThree') {
+    questionsWrong();
+    location.href = 'index.html';
+  } else {
+    incorrectNumber++;
+    console.log(incorrectNumber);
+    confirm('You got it wrong fool. Pick again');
   }
 };
 
@@ -43,26 +71,23 @@ var questionOneEl = function() {
   var questionEl = document.getElementById('question');
   question.textContent = questionOne;
 
-  var answerEl = document.getElementById('possibleAnswers');
+  var questionOneAnswerOneEl = document.createElement('li');
+  questionOneAnswerOneEl.setAttribute('id', 'correctAnswerOne');
+  answerEl.appendChild(questionOneAnswerOneEl);
+  questionOneAnswerOneEl.textContent = answerOne;
+  questionOneAnswerOneEl.addEventListener('click', handleClick);
 
-  var answerOneEl = document.createElement('li');
-  answerOneEl.setAttribute('id', 'correctAnswer');
-  console.log(answerOneEl.id);
-  answerEl.appendChild(answerOneEl);
-  answerOneEl.textContent = answerOne;
-  answerOneEl.addEventListener('click', handleClick);
+  var questionOneAnswerTwoEl = document.createElement('li');
+  questionOneAnswerTwoEl.setAttribute('id', 'incorrectAnswerOne');
+  answerEl.appendChild(questionOneAnswerTwoEl);
+  questionOneAnswerTwoEl.textContent = answerOneIncorrectOne;
+  questionOneAnswerTwoEl.addEventListener('click', handleClick);
 
-  var answerTwoEl = document.createElement('li');
-  answerTwoEl.setAttribute('id', 'incorrectAnswer');
-  answerEl.appendChild(answerTwoEl);
-  answerTwoEl.textContent = answerOneIncorrectOne;
-  answerTwoEl.addEventListener('click', handleClick);
-
-  var answerThreeEl = document.createElement('li');
-  answerThreeEl.setAttribute('id', 'incorrectAnswer');
-  answerEl.appendChild(answerThreeEl);
-  answerThreeEl.textContent = answerOneIncorrectTwo;
-  answerThreeEl.addEventListener('click', handleClick);
+  var questionOneAnswerThreeEl = document.createElement('li');
+  questionOneAnswerThreeEl.setAttribute('id', 'incorrectAnswerTwo');
+  answerEl.appendChild(questionOneAnswerThreeEl);
+  questionOneAnswerThreeEl.textContent = answerOneIncorrectTwo;
+  questionOneAnswerThreeEl.addEventListener('click', handleClick);
 
 };
 
@@ -72,13 +97,56 @@ var questionTwoEl = function() {
   var questionEl = document.getElementById('question');
   question.textContent = questionTwo;
 
+  var questionTwoAnswerOneEl = document.getElementById('correctAnswerOne');
+  questionTwoAnswerOneEl.setAttribute('id', 'incorrectAnswerOne');
+  answerEl.appendChild(questionTwoAnswerOneEl);
+  questionTwoAnswerOneEl.textContent = answerTwoIncorrectOne;
+  questionTwoAnswerOneEl.addEventListener('click', handleClick);
+
+  var questionTwoAnswerTwoEl = document.getElementById('incorrectAnswerOne');
+  questionTwoAnswerTwoEl.setAttribute('id', 'correctAnswerTwo');
+  answerEl.appendChild(questionTwoAnswerTwoEl);
+  questionTwoAnswerTwoEl.textContent = answerTwo;
+  questionTwoAnswerTwoEl.addEventListener('click', handleClick);
+
+  var questionTwoAnswerThreeEl = document.getElementById('incorrectAnswerTwo');
+  questionTwoAnswerThreeEl.setAttribute('id', 'incorrectAnswerTwo');
+  answerEl.appendChild(questionTwoAnswerThreeEl);
+  questionTwoAnswerThreeEl.textContent = answerTwoIncorrectTwo;
+  questionTwoAnswerThreeEl.addEventListener('click', handleClick);
+
 };
 
 // Question Three
 
 var questionThreeEl = function() {
   var questionEl = document.getElementById('question');
-  question.textContent = questionTwo;
+  question.textContent = questionThree;
+
+  var questionThreeAnswerOneEl = document.getElementById('incorrectAnswerTwo');
+  questionThreeAnswerOneEl.setAttribute('id', 'incorrectAnswerOne');
+  answerEl.appendChild(questionThreeAnswerOneEl);
+  questionThreeAnswerOneEl.textContent = answerThreeIncorrectOne;
+  questionThreeAnswerOneEl.addEventListener('click', handleClick);
+
+  console.log(incorrectNumber);
+
+  var questionThreeAnswerTwoEl = document.getElementById('incorrectAnswerOne');
+  questionThreeAnswerTwoEl.setAttribute('id', 'incorrectAnswerTwo');
+  answerEl.appendChild(questionThreeAnswerTwoEl);
+  questionThreeAnswerTwoEl.textContent = answerThreeIncorrectTwo;
+  questionThreeAnswerTwoEl.addEventListener('click', handleClick);
+
+  console.log(incorrectNumber);
+
+  var questionThreeAnswerThreeEl = document.getElementById('correctAnswerTwo');
+  questionThreeAnswerThreeEl.setAttribute('id', 'correctAnswerThree');
+  answerEl.appendChild(questionThreeAnswerThreeEl);
+  questionThreeAnswerThreeEl.textContent = answerThree;
+  questionThreeAnswerThreeEl.addEventListener('click', handleClick);
+
+  console.log(incorrectNumber);
+
 };
 
 questionOneEl();
